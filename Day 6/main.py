0406c8f9d3691d8f8e2f805ab8805c7ed3f1938c
Hem Parekh <hemparekh@Hems-MacBook-Pro.local>
2022-09-16 10:01:45 -0400

    Day 6

---
+++ Day 6/main.py
@@ -0,0 +1,11 @@
+def turn_right():
+    for i in range(0,3):
+        turn_left()    
+while not at_goal():
+    if not wall_on_right():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
