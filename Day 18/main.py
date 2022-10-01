832b41bbedaeafb44248088988090edc0408ceee
Hem Parekh <hemparekh@Hems-MacBook-Pro.local>
2022-10-01 16:38:30 -0400

    Day 18

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,28 +1,34 @@
 import random
 import turtle
 from turtle import Turtle, Screen
+import colorgram
+
 
 colors = ['CornflowerBlue', 'DarkOrchid', 'IndianRed', 'DeepSkyBlue', 'wheat', 'LightSeaGreen',
           'SlateGray', 'SeaGreen']
 directions = [0, 90, 180, 270]
 timmy = Turtle()
+turtle.colormode(255)
 timmy.speed('fastest')
-# # timmy.forward(100)
-# # timmy.left(90)
-# # timmy.forward(100)
-# # timmy.left(90)
-# # timmy.forward(100)
-# # timmy.left(90)
-# # timmy.forward(100)
-# # timmy.left(90)
-#
-#
-# for _ in range(10):
-#     timmy.forward(10)
-#     timmy.penup()
-#     timmy.forward(10)
-#     timmy.pendown()
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.left(90)
+for _ in range(10):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
 
 def draw_shape(num_sides):
     for _ in range(num_sides):
@@ -32,14 +38,30 @@ def draw_shape(num_sides):
 
 
 for i in range(3, 11):
-    timmy.color(random.choice(colors))
+    timmy.color(random_color())
     draw_shape(i)
 
 for _ in range(200):
-    timmy.color(random.choice(colors))
+    timmy.color(random_color())
     timmy.pensize(5)
     timmy.forward(30)
     timmy.setheading(random.choice(directions))
+
+def draw_spirogragh(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        timmy.color(random_color())
+        timmy.circle(100)
+        timmy.setheading(timmy.heading()+size_of_gap)
+
+
+
+draw_spirogragh(5)
+
+
+
+
+
+
 
 
 
